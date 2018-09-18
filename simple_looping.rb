ca97e547bd3ea6db9ed85024f53dab06838d74b4
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-18 23:25:31 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,11 +1,6 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  # loop do 
-  #   int = 4
-  
-  #   puts "Welcome to Flatiron School's Web Development Course!"
-  #   if counter 
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
