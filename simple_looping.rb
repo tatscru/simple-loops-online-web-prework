bbe175428a375621cac460ac3cea42679f844729
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-18 23:35:50 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,7 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  loop do 
+  # loop do 
     
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
